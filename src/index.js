cc4d65048d97e51e40b0793e99a6076b83f5132e
asfound <fffound27@gmail.com>
2024-08-27 13:16:32 +0200

    feat: solve task with for loop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // define 'closed' pairs in config
+  const pairs = [];
+  for (let pair of bracketsConfig) {
+    pairs.push(pair.join(""));
+  }
+
+  // copy the str for checking
+  let given = str;
+
+  // check: remove 'closed' pairs to find 'open' stray brackets
+  for (let i = given.length; i > 1; i -= 2) {
+    for (let pair of pairs) {
+      given = given.replace(pair, "");
+    }
+  }
+
+  // check if any 'open' stray brackets left
+  return given.length === 0;
+};
+
+// console.log(
+//   check("[]()", [
+//     ["(", ")"],
+//     ["[", "]"],
+//   ])
+// );
+
+// ("()");
+// ("(())");
+// ("(())()");
+// ("()(");
+// ("())");
+// (")(()");
+// ("()()()");
+
+// ("((()(()()((()))))()))");
